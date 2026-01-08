ba1239b3941257b9063c9d1b633bcd3a400c0ed9
Harshal Waghare <88886066+harshalwaghare18@users.noreply.github.com>
2026-01-08 23:30:00 +0530

    Fix GitHub Pages: move index.html to root

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// const PORT = 4000;
+
+// // Serve static files
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // Serve HTML
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views", "index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Portfolio running at http://localhost:${PORT}`);
+// });
+
+
+
 const express = require("express");
 const path = require("path");
 
@@ -5,11 +25,11 @@ const app = express();
 const PORT = 4000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-// Serve HTML
+// Serve index.html from ROOT
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
